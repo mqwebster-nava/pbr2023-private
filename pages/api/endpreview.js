a59eb575bcb2343e422a2c1cf746db5fa696d96f
connorshannon <connorshannon@navapbc.com>
2022-01-07 12:36:12 -0500

    added code to enable preview

---
+++ pages/api/endpreview.js
@@ -0,0 +1,10 @@
+/*
+ * Clears the Next.js preview mode cookies.
+ * This function accepts no arguments.
+ */
+
+export default function handler(req, res) {
+    res.clearPreviewData();
+    res.redirect("/");
+  }
+  
