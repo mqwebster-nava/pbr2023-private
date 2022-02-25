4acf92404ddaffadce4354eea6b4de298de64091
connorshannon <connorshannon@navapbc.com>
2022-02-25 16:27:25 -0600

    added side nav

--- components/templates/PostTemplate.tsx
+++ components/templates/PostTemplate.tsx
@@ -6,10 +6,14 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
 import PostHeader from 'components/row/PostHeader/PostHeader';
 import {allTagsSlugIdPair} from "utils/utils";
-
+import { useEffect, useState } from "react";
+import AnchorLink from "components/templates/AnchorLink";
+ 
 export default function PostTemplate({post, morePosts, preview }) {
   // need to deconstruct post 
-    const getImg= (data:any) => {
+  const doc = post.body.json;
+  
+   const getImg= (data:any) => {
       const id = data.target.sys.id;
       const assets = post.body.links.assets.block;
       const asset = assets.find(element => element.sys.id === id);
@@ -19,13 +23,15 @@ export default function PostTemplate({post, morePosts, preview }) {
     const options = {
       renderMark: {
         [MARKS.BOLD]: text => <span className="text-blue-500 font-bold ">{text}</span>,
-        [MARKS.ITALIC]: text => <span className="text-blue-500 font-bold">{text}</span>,
+        [MARKS.ITALIC]: text => <span className="italic font-sans">{text}</span>,
         [MARKS.UNDERLINE]: text => <span className="text-blue-500 font-bold">{text}</span>,
       },
       renderNode: {
         [BLOCKS.PARAGRAPH]: (node, children) => <p className="text-black font-sans py-md text-base">{children}</p>,
         [BLOCKS.HEADING_1]: (node, children) => <p className="text-3xl font-bold font-sans pt-md">{children}</p>,
-        [BLOCKS.HEADING_2]: (node, children) => <p className=" text-2xl font-bold font-sans pt-md">{children}</p>,
+        [BLOCKS.HEADING_2]: (node, children) =>{
+          return ( <h2 id={children[0]} className="text-2xl font-bold font-sans pt-md">{children}</h2>)
+        },
         [BLOCKS.HEADING_3]: (node, children) => <p className="text-xl font-bold font-sans">{children}</p>,
         [BLOCKS.HEADING_4]: (node, children) => <p className="text-blue-500">{children}</p>,
         [BLOCKS.UL_LIST]: (node, children) => <ul className="list-disc ml-2xl">{children}</ul>,
@@ -35,22 +41,14 @@ export default function PostTemplate({post, morePosts, preview }) {
         ),
         [BLOCKS.EMBEDDED_ASSET]: ({ data }) => getImg(data)
       },
-
   };
-  
-   const doc = post.body.json;
  
-    return (
-      <div >
-        <PostHeader title={post.title} long_summary={post.longSummary??post.shortSummary} content_type={post.contentType}/>
-        <div className={"px-3xl xl:px-0 xl:mx-auto xl:max-w-screen-lg my-2xl flex"}>
-       <div className="w-2/3 pr-2">
 
-            {documentToReactComponents(doc, options)}
-          </div>
-          <div className={"w-1/3"}>
-            <p>Date: ______</p>
-            
+
+ 
+const ArticleInfoComponent = () =>{
+  return (<div>
+    <p>Date: ______</p>
             <div>
               <h3>Authors</h3>
               {post.authorsCollection.items.map((author)=>(
@@ -67,8 +65,36 @@ export default function PostTemplate({post, morePosts, preview }) {
                 <p id={tag}><a className="text-blue-700 underline" href={`/tags/${allTagsSlugIdPair.revGet(tag)}`}>{tag}</a></p>
               )})}
             </div>
+  </div>)
+}
+const SideNavComponent = () =>{
+  const headers = doc.content.filter(node => node.nodeType=="heading-2").map(node=>node.content[0].value);
+  console.log(headers);
+  return ( 
+  <div className="mt-md  p-lg sticky top-48 border-2 w-fit">
+    <h3>On This Page</h3>
+    {
+        headers.map((header)=>{
+          return ( <p className="font-sans text-blue-500 hover:text-blue-900"><AnchorLink href={`#${header}`}>{header}</AnchorLink> </p>)
+        })
+    }
+  </div>
+  )
+}
+
+    return (
+      <div >
+        <PostHeader title={post.title} long_summary={post.longSummary??post.shortSummary} content_type={post.contentType}/>
+        <div className={"px-3xl xl:px-0 xl:mx-auto xl:max-w-screen-lg my-2xl flex"}>
+        {/* Article Body Section  */}
+        <div id="article" className="w-2/3 pr-2">
+            {documentToReactComponents(doc, options)}
           </div>
-         
+          {/* Article Sidebar Section  */}
+          <div className={"w-1/3"}>
+            <ArticleInfoComponent/>
+            <SideNavComponent/>
+          </div>
         </div>
       </div>
     );
