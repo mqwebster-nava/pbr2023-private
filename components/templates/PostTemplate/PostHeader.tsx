26184eabec5f3147b60722ab024cefe443b0d011
connorshannon <connorshannon@navapbc.com>
2022-03-08 17:17:14 -0600

    header

--- components/templates/PostTemplate/PostHeader.tsx
+++ components/templates/PostTemplate/PostHeader.tsx
@@ -16,16 +16,16 @@ const PostHeader = ({
 }: PostHeaderProps) => {
   return (
     <div>
-      <div className="bg-gray-300 w-screen pb-4xl">
+      <div className="bg-gray-300 w-screen pb-[160px] pt-2xl">
         <div className=" py-4xl px-3xl xl:px-0 xl:mx-auto xl:max-w-screen-lg ">
           <h3 className="pt-2xl font-sans font-light text-black"> {contentType}</h3>
-          <h1 className="font-sans text-4xl font-black text-black"> {title}</h1>
+          <h1 className="font-sans lg:text-4xl text-3xl font-black text-black"> {title}</h1>
           <p className="font-sans font-light pt-lg text-black">
             {longSummary}
           </p>
         </div>
       </div>
-      <div className=" max-h-96 overflow-clip xl:mx-auto xl:max-w-screen-lg -mt-4xl"> 
+      <div className=" max-h-96 overflow-clip xl:mx-auto xl:max-w-screen-lg -mt-[160px]"> 
         <Image className="max-w-full max-h-16" src={filler}></Image>
       </div>
     </div>
