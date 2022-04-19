a50d55f8f5e7d9c3814be6417c3e9be764ba3513
connorshannon <connorshannon@navapbc.com>
2022-04-19 16:51:34 -0700

    updated links text to default to being black

--- components/atom/LinkText/LinkText.tsx
+++ components/atom/LinkText/LinkText.tsx
@@ -6,6 +6,6 @@ export interface  LinkTextProps  {
 export const LinkText: React.FC<LinkTextProps> = ({ children, href }) => {
   const target = (href.substr(0,1)==="/") ? "" : "_blank"
   return (
-    <a className={`text-sage-dark hover:text-sage-base underline`} href={href}  target={target}  >{children}</a>
+    <a className={`text-black hover:text-sage-base underline`} href={href}  target={target}  >{children}</a>
   )
 }
