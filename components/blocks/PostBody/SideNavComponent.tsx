c8a4fb8ab261e94f4bef8d70f449fd859b16e5cf
connorshannon <connorshannon@navapbc.com>
2022-04-29 11:38:21 -0700

    updated sidenav style

--- components/blocks/PostBody/SideNavComponent.tsx
+++ components/blocks/PostBody/SideNavComponent.tsx
@@ -1,17 +1,24 @@
 import AnchorLink from "components/atom/AnchorLink/AnchorLink";
-
+import classNames from "classnames";
 
 const SideNavComponent = ({h2Sections, activeSection}) => {
 
+
+
+
   return (
-    <div className="mt-md  p-lg sticky top-48 border-2 w-full max-w-[240px]  hidden md:block bg-white">
-      <h3 className="font-sans font-bold">On This Page</h3>
-      <hr/>
+    <div className="mt-md   sticky top-48 w-full max-w-[240px]  hidden md:block ">
+      <h3 className="font-sans font-bold pb-sm">On this page</h3>
+  
       {h2Sections.map((section) => {
         let header = section.title;
         let isActive = activeSection === header;
+        const style = classNames({
+          "text-sage-700 border-l-4 border-sage-700":isActive,
+          "border-l-2 border-sage-50":!isActive
+        });
         return (
-          <p className={`font-sans text-sage-dark hover:text-sage-base ${isActive&&"font-bold"}`}>
+          <p className={`font-sans text-sage-dark  pl-sm py-sm hover:text-sage-base ${style}`}>
             <AnchorLink href={`#${header}`}>{header}</AnchorLink>
           </p>
         );
