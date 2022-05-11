b31c7dd5656f0720a02d6f572c1c7e9c6849c6b4
connorshannon <connorshannon@navapbc.com>
2022-05-10 17:31:36 -0700

    tailwind

---
+++ tests/api.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import { axe, toHaveNoViolations } from "jest-axe";
+
+const pages = []
+
+
+
+
+describe("API Test", () => {
+   
+    it("matches pages", async () => {
+     
+    });
+});
+  
