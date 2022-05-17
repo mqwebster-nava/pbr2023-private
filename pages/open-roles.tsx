d603beae08ae3545cc411342bcc92030a7a2b685
connorshannon <connorshannon@navapbc.com>
2022-05-17 10:09:54 -0700

    updated open roles, moved component out

--- pages/open-roles.tsx
+++ pages/open-roles.tsx
@@ -1,20 +1,7 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-//https://www.digitalocean.com/community/tutorials/react-axios-react
-import AnchorLink from "components/atom/AnchorLink/AnchorLink";
-import { LinkText } from "components/atom/LinkText/LinkText";
 import { PageInterface } from "shared_interfaces/page_interface";
 import { getPageDataFromContentful } from "lib/api";
 import PageTemplate from "components/templates/PageTemplate/PageTemplate";
 import OpenRolesComponent from "components/custom_blocks/OpenRolesComponent/OpenRolesComponent";
-/*
-In the open positions page, we are getting Nava's open positions from Lever's API,
-since all the roles are updated there.
-
-We use axios to call the api at page load and display them in groupings.
-
-*/
 
 
 export default function OpenRoles(props:PageInterface) {
@@ -30,7 +17,7 @@ export default function OpenRoles(props:PageInterface) {
 export async function getStaticProps({ params, preview = false }) {
   const res: PageInterface = 
     await getPageDataFromContentful({
-      slug:"/", 
+      slug:"/open-roles", 
       preview: preview,
     });
   return {
