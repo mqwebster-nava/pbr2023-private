587b7cf49fa5c5d8c5bcd81061a41042dab9f63e
connorshannon <connorshannon@navapbc.com>
2022-05-17 10:48:23 -0700

    removed highlight card

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,7 @@ import HighlightedCard from "components/atom/ContentCard/HighlightedCard";
 
 export default function Home(props:PageInterface) {
   return props ? (
-    <PageTemplate {...props}>
-
-<HighlightedCard 
-      title={"How APIs can help WIC better meet staff and participants’ needs"} 
-      path={"/" } 
-      type={"Insight"}
-     promoImage={ 
-      {
-        id: '5D9BooUBjHC6MYX45Xyk7T',
-        url: 'https://images.ctfassets.net/t2ekr6eg3fr3/6rpVAwgdjrrnR2OrgaHzrL/8a192fb130c0dba481ca54562deb022d/sample-feature.png?h=250',
-        title: 'How APIs can help WIC better meet staff and participants’ needs',
-        description: 'An API standard would unlock digital tools and catalyze WIC’s ability to evolve and grow.',
-        width: 1500,
-        height: 845}  
-      }>
-        An API standard would unlock digital tools and catalyze WIC’s ability to evolve and grow.
-      </HighlightedCard>
-      
-    </PageTemplate>
+    <PageTemplate {...props}></PageTemplate>
   ) : (
     <div>Error</div>
   );
