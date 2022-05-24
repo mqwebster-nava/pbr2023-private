741c4d590b562f1e240d2c338c6c5ff6731f43eb
connorshannon <connorshannon@navapbc.com>
2022-05-24 16:27:53 -0700

    fixed caption text bug

--- components/blocks/PostBody/CaptionText.tsx
+++ components/blocks/PostBody/CaptionText.tsx
@@ -1,3 +1,5 @@
+import { MarkdownComponent } from "utils/utils";
+
 interface CaptionTextInterface {
     id:string;
   caption: string;
@@ -7,8 +9,8 @@ interface CaptionTextInterface {
 const CaptionText = ({ id, caption, attribution }: CaptionTextInterface) => {
   return (
     <div className={"border-l-black border-l-2 pl-md mb-xl mt-sm"} key={id}>
-      <p className="type-preset-7 font-sans">{caption}</p>
-      {attribution ?? <p className="type-preset-7 font-sans text-gray-400">{attribution}</p>}
+      <p className="type-preset-7 font-sans">{<MarkdownComponent content={caption}/>}</p>
+      {attribution && <p className="type-preset-7 font-sans text-gray-400 pt-md">{attribution}</p>}
     </div>
   );
 };
