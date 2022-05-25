776f78a3e2c38b14c52d5db45894a9fc17df1411
connorshannon <connorshannon@navapbc.com>
2022-05-25 16:58:31 -0700

    Updated cta block

--- components/blocks/CTABlock/CTABlock.tsx
+++ components/blocks/CTABlock/CTABlock.tsx
@@ -1,8 +1,13 @@
 import { Button } from "../../atom/Button/Button";
 import { MarkdownComponent } from "utils/utils";
-import ill from "public/images/Illustrations-for-website-V019 1.png";
+import sageIll from "public/images/Illustrations-for-website-V019 1.png";
+import plumIll from "public/images/plum cta illustration.png";
 import Image from "next/image";
 import { ContentfulImageAsset } from "shared_interfaces/post_interface";
+import classNames from "classnames";
+
+
+type CTAColorTheme =   "plum" | "sage";
 interface CTABlockInterface {
   id: string;
   title: string;
@@ -10,7 +15,7 @@ interface CTABlockInterface {
   image: ContentfulImageAsset;
   buttonText: string;
   buttonPath: string;
-  type: string;
+  colorTheme: CTAColorTheme
 }
 
 
@@ -21,20 +26,30 @@ const CTABlock = ({
   image,
   buttonPath,
   buttonText,
+  colorTheme="plum"
 }:CTABlockInterface) => {
+
+  const bgColor= classNames({"bg-plum-50": colorTheme=="plum","bg-sage-50": colorTheme=="sage",})
+  const headerColor= classNames({"text-plum-900": colorTheme=="plum","text-sage-900": colorTheme=="sage",})
+  const subtitleColor= classNames({"text-plum-600": colorTheme=="plum","text-sage-600": colorTheme=="sage",})
+  const layout= classNames({"flex-row-reverse": colorTheme=="plum"})
+  
+  const img = colorTheme=="plum"?plumIll:sageIll;
+
   return (
-    <div className=" w-full bg-sage-50 " key={id}>
-      <div className="responsive-container flex items-center">
+    <div className={` w-full ${bgColor}`}  key={id}>
+      <div className={`responsive-container flex items-center flex-row-reverse ${layout}`} >
       <div className="w-1/2 max-h-[500px] max-w-[500px]">
           <Image
-            className=" object-cover "
-            src={image?image.url:ill}
+            src={img}
             layout="responsive"
+            height={500}
+            width={500}
           ></Image>
         </div>
-        <div className={`w-full lg:w-1/2 px-lg py-md`}>
-          <h3 className=" type-preset-2 font-bold  text-sage-600">{title} </h3>
-          <p className={`pt-lg  type-preset-2 font-bold text-sage-900 mb-lg`}>
+        <div className={`w-full lg:w-1/2 px-lg py-lg`}>
+          <h3 className={` type-preset-2 font-bold  ${headerColor}`}>{title} </h3>
+          <p className={`pt-lg  type-preset-2 font-bold ${subtitleColor} mb-lg`}>
             {<MarkdownComponent content={body} />}
           </p>
           {buttonText && (
