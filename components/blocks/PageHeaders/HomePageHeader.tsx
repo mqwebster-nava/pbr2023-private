738d3705f8464e3986e516ea475873bb98b8ab39
connorshannon <connorshannon@navapbc.com>
2022-05-27 10:30:17 -0700

    progress on home page- need mobile

--- components/blocks/PageHeaders/HomePageHeader.tsx
+++ components/blocks/PageHeaders/HomePageHeader.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import ill from "public/images/homeheader1.svg";
 import pat from "public/images/HeaderPattern2.svg";
+import { Button } from "components/atom";
 
 const defaultIllustration: ContentfulImageAsset = {
   id: "2noBSMs0a4cEnhGkcWIxdC",
@@ -25,30 +26,28 @@ const defaultPattern: ContentfulImageAsset = {
 const HomePageHeader = ({ id, title }: PageHeaderInterface) => {
   return (
     <header key={id}>
-      <div className={`bg-plum-900  `}>
-        <div className={`responsive-container relative z-10  pt-5xl pb-4xl`}>
-        <div className="absolute z-5 top-0 right-0  w-1/2 h-full pr-xl md:pr-4xl 2xl:pr-0 2xl:mx-auto 2xl:max-w-screen-xl ">
+      <div className={`bg-plum-900`}>
+        <div className={`responsive-container relative z-10  `}>
+        <div className="z-5 w-full sm:w-3/4 lg:w-1/2 ml-auto ">
               <Image
-                className="w-1/2 object-cover "
+                className="object-cover "
                 src={defaultPattern.url}
                 width={defaultPattern.width}
                 height={defaultPattern.height}
                 layout="responsive"
               ></Image>
-         
           </div>
-          <h1 className={`font-sans type-preset-1  relative z-10 text-white font-black`}>
-            {title}
-          </h1>
-
-          
+          <div className={` absolute  z-10 top-[72px] lg:top-[128px]  h-full pr-xl md:pr-4xl 2xl:pr-0 2xl:mx-auto 2xl:max-w-screen-xl `}>
+            <h1 className={`font-sans type-preset-1 text-white font-black pb-md`}>{title}</h1>
+            <Button variant="white" href={"/services"}>Our Work</Button>
+          </div>
         </div>
       </div>
       <div className=" relative responsive-container ">
-        <div className="bg-plum-900 -mt-[3px]  h-1/5 w-full absolute z-0 top-0 left-0 lg:left-1/2 lg:w-screen lg:-translate-x-1/2 "></div>
-        <div className="w-1/2 ml-auto">
+        <div className="bg-plum-900 -mt-[3px]  h-1/2 w-full absolute z-0 top-0 left-0 lg:left-1/2 lg:w-screen lg:-translate-x-1/2 "></div>
+        <div className="w-3/4 lg:w-1/2 ml-auto">
           <Image
-            className="w-1/2 object-cover "
+            className="object-cover "
             src={defaultIllustration.url}
             width={defaultIllustration.width}
             height={defaultIllustration.height}
