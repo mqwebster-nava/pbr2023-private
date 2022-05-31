18536e542f731b5c5231166dfd8341afc82069b2
connorshannon <connorshannon@navapbc.com>
2022-05-31 09:57:58 -0700

    added icon

--- components/atom/FeatureCard/FeatureCard.tsx
+++ components/atom/FeatureCard/FeatureCard.tsx
@@ -1,5 +1,5 @@
 import { ContentfulImageAsset } from "shared_interfaces/post_interface";
-
+import Image from "next/image";
 interface FeatureCardInterface {
     id?: string;
     icon?: ContentfulImageAsset; // TODO what to do with the icon selection (?),
@@ -7,10 +7,29 @@ interface FeatureCardInterface {
     body: string;
 }
 
+const defaultIcon:ContentfulImageAsset = 
+    {
+       
+        "id": "3hBHof0lL9fl2Ui7QQdGnD",
+        "url": "https://images.ctfassets.net/t2ekr6eg3fr3/3hBHof0lL9fl2Ui7QQdGnD/031cc4c1e72fb98b335ae54f3e2840eb/icon-house.svg",
+        "width": 38,
+        "height": 38,
+        "title": "icon-house",
+        "description": "Icon of a house"
+    }
+
+
 const FeatureCard = ({id, icon, title, body}:FeatureCardInterface) => {
+   
+    icon = icon ?? defaultIcon;
     return (
     <div className="bg-navy-900 px-xl py-4xl h-full ">
-        <p className={"type-preset-4 font-sans font-bold text-white"}>
+          <Image 
+            src={icon.url} 
+            height={"36px"}
+            width={"36px"}
+            ></Image>
+        <p className={"type-preset-4 font-sans font-bold text-white pt-md"}>
             <span className="text-purple-100 ">{`${title} `}</span>
             {body}
         </p>
