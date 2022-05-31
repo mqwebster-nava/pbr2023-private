f13f31a39c4d9bedb5b3190e47335557cff49b51
connorshannon <connorshannon@navapbc.com>
2022-05-31 15:28:09 -0700

    added lang

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 module.exports = withBundleAnalyzer({
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
     images: {
       domains: ['images.ctfassets.net'],
     },
