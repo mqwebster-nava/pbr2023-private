3be185a3bb8357baa070464d16b0607451bea262
connorshannon <connorshannon@navapbc.com>
2022-05-31 15:27:19 -0700

    fixed caption text

--- components/blocks/PostBody/CaptionText.tsx
+++ components/blocks/PostBody/CaptionText.tsx
@@ -7,9 +7,9 @@ interface CaptionTextInterface {
 
 const CaptionText = ({ id, caption, attribution }: CaptionTextInterface) => {
   return (
-    <div className={"border-l-black border-l-2 pl-md mb-xl mt-sm"} key={id}>
-      <p className="type-preset-7 font-sans">{caption}</p>
-      {attribution && <p className="type-preset-7 font-sans text-gray-400 pt-md">{attribution}</p>}
+    <div className={"type-preset-7 border-l-black border-l-2 pl-md mb-xl mt-sm"} key={id}>
+      <p className=" font-sans">{caption}</p>
+      {attribution && <p className=" font-sans text-gray-400 pt-md">{attribution}</p>}
     </div>
   );
 };
