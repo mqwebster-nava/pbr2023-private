734ca4625efb11fa74f72bbf6402ae382c0e0adf
connorshannon <connorshannon@navapbc.com>
2022-05-31 20:13:52 -0700

    quote set up

--- components/blocks/ReportBody/PullQuote/PullQuote.tsx
+++ components/blocks/ReportBody/PullQuote/PullQuote.tsx
@@ -1,7 +1,7 @@
 export const PullQuote = ({ children }) => {
   return (
     <blockquote className="py-xl text-sage-900 before:content-none">
-      <img src="/images/pbrs/quote_mark.svg" alt="" />
+      <img src="/images/pbrs/quote_mark.svg" alt=""  />
       {children}
     </blockquote>
   );
