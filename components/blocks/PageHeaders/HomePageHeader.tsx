a147fabe8976d87cfaebbc83458de6699fd2526a
connorshannon <connorshannon@navapbc.com>
2022-06-10 11:55:51 -0700

    Updates to home page header

--- components/blocks/PageHeaders/HomePageHeader.tsx
+++ components/blocks/PageHeaders/HomePageHeader.tsx
@@ -25,14 +25,14 @@ const HomePageHeader = ({ id, title }: PageHeaderInterface) => {
         <div className={`responsive-container relative z-30  `}>
           <div className="w-full sm:w-2/3 md:w-3/5 lg:w-3/5 ml-auto grid grid-cols-3">
             <img
-              className="object-cover w-full col-span-3 lg:col-span-2"
+              className="object-cover w-full col-span-3 sm:col-span-2 md:col-span-3 lg:col-span-2 lg:max-h-full max-h-[350px] "
               src={header.pattern}
               alt={""}
               height="300"
               width="300"
             ></img>
             <img
-              className="object-cover w-full col-span-1 hidden lg:block"
+              className="object-cover w-full col-span-1  hidden sm:block md:hidden lg:block"
               src={header.pattern2}
               alt={""}
               height="300"
@@ -42,10 +42,10 @@ const HomePageHeader = ({ id, title }: PageHeaderInterface) => {
             
           </div>
           <div
-            className={` absolute  z-40 top-[72px] md:top-[100px] lg:top-[128px]  h-full pr-xl md:pr-2xl 2xl:pr-0 2xl:mx-auto 2xl:max-w-screen-xl `}
+            className={` absolute  z-40 top-[80px] md:top-[80px] lg:top-[128px]  h-full pr-xl md:pr-2xl 2xl:pr-0 2xl:mx-auto 2xl:max-w-screen-xl `}
           >
             <h1
-              className={`animate-titleSlide font-sans type-preset-1 text-white font-black pb-md w-full lg:w-3/4`}
+              className={`animate-titleSlide font-sans type-preset-1 text-white font-black pb-md w-full sm:w-3/4 md:w-full lg:w-3/4`}
             >
               {title}
             </h1>
