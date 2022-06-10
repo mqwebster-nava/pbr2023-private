21a9d8395f385650129f27301fdee2dd965114d0
connorshannon <connorshannon@navapbc.com>
2022-06-09 17:00:36 -0700

    attempt on fixing cta block

--- components/blocks/CTABlock/CTABlock.tsx
+++ components/blocks/CTABlock/CTABlock.tsx
@@ -29,13 +29,22 @@ const CTABlock = ({
   const bgColor= classNames({"bg-plum-50": colorTheme=="plum","bg-sage-50": colorTheme=="sage",})
   const headerColor= classNames({"text-plum-900": colorTheme=="plum","text-sage-900": colorTheme=="sage",})
   const subtitleColor= classNames({"text-plum-600": colorTheme=="plum","text-sage-600": colorTheme=="sage",})
-  const layout= classNames({"flex-row-reverse": colorTheme=="plum"})
+  //const layout= classNames({"flex-row-reverse": colorTheme=="plum"})
   
 
   return (
     <section className={` w-full ${bgColor}`}  key={id}>
-      <div className={`responsive-container flex justify-start items-center ${layout}`} >
-      <div className="w-1/2 max-h-[500px] max-w-[500px]">
+      <div className={`responsive-container pt-md flex flex-col md:flex-row justify-start md:items-center items-start`} >
+      <div className={`w-full md:w-1/2  pt-3xl pb-md md:pt-4xl md:pb-3xl md`}>
+          <h3 className={` type-preset-3 font-bold  ${headerColor}`}>{title} </h3>
+          <p className={`pt-sm  type-preset-3 font-bold ${subtitleColor} pb-2xl`}>
+            {body}
+          </p>
+          {buttonText && (
+            <Button href={buttonPath} variant="white" children={buttonText}/> 
+          )}
+        </div>
+      <div className="w-full md:w-1/2 max-h-[400px] md:max-h-[500px] max-w-[500px] overflow-hidden">
           <Image
             src={image.url}
             layout="responsive"
@@ -44,15 +53,7 @@ const CTABlock = ({
             alt={image.description} // TODO add alt image names for the illustrations
           ></Image>
         </div>
-        <div className={`w-full lg:w-1/2 px-lg py-lg`}>
-          <h3 className={` type-preset-2 font-bold  ${headerColor}`}>{title} </h3>
-          <p className={`pt-lg  type-preset-2 font-bold ${subtitleColor} mb-lg`}>
-            {body}
-          </p>
-          {buttonText && (
-            <Button href={buttonPath} variant="white" children={buttonText}/> 
-          )}
-        </div>
+      
         
       </div>
     </section>
