081897dac2355bf30204968d7991d096b350f174
connorshannon <connorshannon@navapbc.com>
2022-06-14 09:04:46 -0600

    Created feature article

---
+++ components/blocks/FeaturedArticle/FeaturedArticle.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { ContentfulImageAsset } from "shared_interfaces/post_interface";
+
+interface  FeaturedArticleInterface {
+    title: string;
+    path: string;
+    summary?: any;
+    kicker?:string;
+    promoImage?: ContentfulImageAsset;
+}
+
+const  FeaturedArticle = ({
+  title,
+  path,
+  summary,
+  kicker,
+  promoImage
+}: FeaturedArticleInterface) => {
+ 
+  return (
+    <section className={`w-full`}>
+      <a href={path} className="group responsive-container py-2xl md:py-4xl grid grid-cols-3 ">
+      <div className={`group-hover:brightness-75 col-span-3 md:col-span-2`}>
+        <Image src={promoImage.url}  />
+       </div>
+        <div className={`col-span-3 md:col-span-1 md:px-lg`}>
+          <div className="">
+            {kicker && <small className="text-gray-500 type-preset-7">{kicker}</small>}
+            <p
+              className={`font-sans type-preset-4  text-black group-hover:text-sage-400 font-bold py-md`}
+            >
+             {title}
+            </p>
+            <p className={`font-sans type-preset-6 text-black `}>
+             {summary}
+            </p>
+          </div>
+        </div>
+      </a>
+    </section>
+  );
+};
+
+export default  FeaturedArticle;
