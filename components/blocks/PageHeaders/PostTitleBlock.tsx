c368e187d1850b1b93a89a61405fde3e1ef94678
connorshannon <connorshannon@navapbc.com>
2022-06-16 09:50:58 -0600

    Caping title block width at 9 cols

--- components/blocks/PageHeaders/PostTitleBlock.tsx
+++ components/blocks/PageHeaders/PostTitleBlock.tsx
@@ -4,16 +4,17 @@ import React from "react";
 const TitleBlock = ({ title, subtitle, body, textColor, buttonPath=null, buttonText=null }) => {
     return (
       <div >
-        <h1 className={` font-sans type-preset-1  ${textColor} font-bold md:font-black`}>
+        <h1 className={` font-sans type-preset-1  ${textColor} font-bold md:font-black w-full xl:w-3/4`}>
           {title}
         </h1>
         <p
-          className={` text-white font-sans  type-preset-1 sm:pr-xl pr-0 md:pr-4xl font-bold md:font-black mr-xl pb-md`}
+          className={` text-white type-preset-1 font-sans font-bold md:font-black
+                        w-full  xl:w-3/4 pb-md`}
         >
           {subtitle}
         </p>
         {body && (
-          <p className="font-serif font-light type-preset-4 py-lg text-white">
+          <p className="font-serif font-light type-preset-4 py-lg text-white w-full xl:w-3/4">
             {body}
           </p>
         )}
--- components/blocks/PageHeaders/TitleBlock.tsx
+++ components/blocks/PageHeaders/TitleBlock.tsx
@@ -3,11 +3,11 @@ import { Button } from "components/atom";
 const TitleBlock = ({ title, subtitle, body, textColor, buttonPath=null, buttonText=null }) => {
     return (
       <div >
-        <h1 className={` font-sans type-preset-1  ${textColor} font-bold md:font-black`}>
+        <h1 className={` font-sans type-preset-1  ${textColor} font-bold md:font-black w-full xl:w-3/4`}>
           {title}
         </h1>
         <p
-          className={` text-white font-sans  type-preset-1 sm:pr-xl pr-0 md:pr-4xl font-bold md:font-black mr-xl pb-md`}
+          className={` text-white font-sans  type-preset-1  font-bold md:font-black pb-md w-full xl:w-3/4`}
         >
           {subtitle}
         </p>
