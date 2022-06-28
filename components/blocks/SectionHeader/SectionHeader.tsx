abc2bc6d7afb6513c46abf4487afffff2d4feefc
connorshannon <connorshannon@navapbc.com>
2022-06-28 10:28:53 -0600

    updated line colors

--- components/blocks/SectionHeader/SectionHeader.tsx
+++ components/blocks/SectionHeader/SectionHeader.tsx
@@ -30,13 +30,20 @@ const SectionHeader = ({
     "bg-navy-900": colorTheme=="navy",
     "bg-purple-900": colorTheme=="purple",
   });
-  const textColor =  (colorTheme=="navy" ||colorTheme=="purple")? "text-white":"text-gray-900";
+  const LineColor = classNames({
+    "bg-gold-900":colorTheme == "gold",
+    "bg-sage-700":colorTheme =="sage",
+    "bg-purple-500":colorTheme=="purple",
+    "bg-navy-200":colorTheme == "navy",
+    "bg-plum-400 ": colorTheme=="default"
+  })
+  const TextColor =  (colorTheme=="navy" ||colorTheme=="purple")? "text-white":"text-gray-900";
 
   const TitleBlock = () => {
     return (
       <div className="lg:w-3/4 w-full">
         <h2
-          className={`w-full font-sans ${textColor} type-preset-2 font-black mt-xl mb-xl`}
+          className={`w-full font-sans ${TextColor} type-preset-2 font-black mt-xl mb-xl`}
         >
           {title}
         </h2>
@@ -46,39 +53,23 @@ const SectionHeader = ({
   const BodyBlock = ({text}) => {
     return (
       <div className={`flex`}>
-        <p className={`lg:w-3/4 type-preset-3  font-serif ${textColor}`}>
+        <p className={`lg:w-3/4 type-preset-3  font-serif ${TextColor}`}>
          {text}
         </p>
       </div>
     );
   };
-  const ColoredLine = () => {
- 
-    return colorTheme == "gold" ? (
-       <div className="bg-gold-900 h-[7px] w-[138px]"></div>
-        
-    ) : colorTheme == "sage" ?  (
-      <div className="bg-sage-900 h-[7px] w-[138px]"></div>
-       
-   ): colorTheme == "purple" ?  (
-    <div className="bg-purple-700 h-[7px] w-[138px]"></div>
-     
- ) : (
-      <div className="bg-plum-400 h-[7px] w-[138px]"></div>
-    );
-  };
-
+  
   return (
     <section className={`${bg}`} key={id}>
     <div className={`responsive-container pt-3xl pb-xl`} key={id}>
       <TitleBlock />
        <div className={`flex ${(subtitle || body) && "mb-xl"}`}>
-         <ColoredLine/>
+       <div className={`${LineColor} h-[5px] w-[65px]`}></div>
         {/* <div className="bg-gray-900 h-[8px] w-[138px]"></div> */}
       </div> 
       {subtitle && <BodyBlock text={subtitle}/>}
       {body && <BodyBlock text={body}/>}
-     
       {buttonText &&<div className="pt-2xl"> <Button  href={buttonPath} variant="white">{buttonText}</Button> </div>}
     </div>
     </section>
