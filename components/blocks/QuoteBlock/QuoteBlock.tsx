2f96dd6636275433ea53771a1d151b8e4065b821
connorshannon <connorshannon@navapbc.com>
2022-06-28 17:53:20 -0600

    updated quote to match comp

--- components/blocks/QuoteBlock/QuoteBlock.tsx
+++ components/blocks/QuoteBlock/QuoteBlock.tsx
@@ -3,8 +3,8 @@ import QuotePattern from "public/images/pattern block.png";
 
 type QuoteBaseColor = "gold" | "purple";
 interface QuoteBlockInterface {
-  id:string;
- body: string;
+  id: string;
+  body: string;
   authorName: string;
   authorRole: string;
   color?: QuoteBaseColor;
@@ -12,19 +12,27 @@ interface QuoteBlockInterface {
 
 const QuoteBlock = ({
   id,
- body,
+  body,
   authorName,
   authorRole,
-  color
-}:QuoteBlockInterface) => {
+  color,
+}: QuoteBlockInterface) => {
   const baseColor = color ? color : "gold";
   return (
     <blockquote className={`bg-gold-700`}>
-      <div className="responsive-container py-2xl md:py-4xl flex flex-wrap">
-      <div className={`w-full pb-md`}>
-         <Image src={QuotePattern} height={70} width={128} layout={"fixed"} alt="quotation marks"></Image>
-       </div>
-        <div className={`w-full lg:w-4/5 flex`}>
+      <div className="responsive-container py-2xl md:py-4xl grid gap-x-2xl grid-cols-6">
+        <div className={`col-span-full md:col-span-1 pb-4xl 
+        h-[56px] w-[100px] lg:h-[72px] lg:w-[128px] 
+        `}>
+          <Image
+            src={QuotePattern}
+            height={70}
+            width={128}
+            layout={"responsive"}
+            alt="quotation marks"
+          ></Image>
+        </div>
+        <div className={`col-span-full md:col-span-5  flex`}>
           <div className="pr-md">
             <p
               className={`font-sans type-preset-3  text-gray-900 font-bold mb-md`}
