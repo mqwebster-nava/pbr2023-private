f56f7b6f5f8e6a76149e80c04b39ee0590d238ac
connorshannon <connorshannon@navapbc.com>
2022-07-06 10:20:53 -0600

    started navbar changes

--- components/wrapper/Navbar/DesktopNav/DesktopNav.tsx
+++ components/wrapper/Navbar/DesktopNav/DesktopNav.tsx
@@ -3,6 +3,31 @@ import Logo from "../Logo";
 import NavButton from "./NavButton";
 import DropdownNavButton from "./DropdownNavButton";
 
+/*
+Notes
+Main Navigation
+Desktop:
+--- Skip to main content button would be nice ( added )
+--- Drop down menus are not buttons - do not voice their state, expanded or not
+--- Drop downs are tabbable, but do not respond to down or up arrow key presses
+--- Focus styling could be better on navigation, borders are cut off
+--- Not sure if issue, but there is a difference in functionality between tabbing and shift-tabbing. Pressing shift-tab does not open the subitems, but tab does. 
+
+Mobile:
+--- Banner is just a banner, there is no screen reader notification that you are in a navigation element
+--- Menu button does not announce expanded or collapsed state
+--- Menu items cannot be accessed via arrow keys (not necessarily an issue)
+
+
+Issues
+- Hidden states lead to issues w/ it finding stuff in the dom
+- Focus state is slightly unclear
+- Slide down makes it harder to implement traditional list view
+- Unsure of what aria labels to add.
+*/
+
+
+
 const DesktopNavBar = ({ NavData }) => {
     return (
       <div className="hidden lg:flex  responsive-container   flex-wrap items-center justify-between h-[80px]">
--- components/wrapper/Navbar/DesktopNav/DropdownNavButton.tsx
+++ components/wrapper/Navbar/DesktopNav/DropdownNavButton.tsx
@@ -3,44 +3,61 @@ import { SlideDown } from "react-slidedown";
 import "react-slidedown/lib/slidedown.css";
 import styles from "./desktopnav.module.css";
 
+
+// Select allows to do the arrows but would have to do a lot of custom styling
+// could just add javascript to handle up and down arrows
+
+// If you use a list item, it helps users know when that is together, 
+//helps users know how many items in that list
+
+// display none leave the containing box while visibility none removes  
+// -- both remove things from the accessibility tree
+
 const DropdownNavButton = (props) => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [isShowingMenu, setIsShowingMenu] = useState(false);
+
+
+  const Chevron = () => (<div className={`chevron mt-[4px]`}>
+  <svg
+    aria-label=""
+    width="8"
+    height="5"
+    viewBox="0 0 20 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M19 1L10 10L1 1"
+      stroke="#111827"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+</div>);
 
   return (
     <div
       className={`${styles.dropdownAnimations} relative group my-auto`}
-      onMouseEnter={() => setShowMenu(true)}
-      onMouseLeave={() => setShowMenu(false)}
+      onMouseEnter={()=>setIsShowingMenu(true)}
+      onMouseLeave={()=>setIsShowingMenu(false)}
+      onFocus={()=>setIsShowingMenu(true)}
+      onBlur={()=>setIsShowingMenu(false)}
     >
       <div
-        className={` dropdowntitle
-        flex flex-row justify-between items-center 
+       tabIndex={0}
+        aria-expanded={isShowingMenu? "true":"false"}
+        className={`dropdowntitle
+        flex flex-row justify-between items-center gap-x-xs
          cursor-pointer mr-3xl
-         pt-md pb-sm
+         pt-md pb-sm 
+         text-gray-600 group-hover:text-gray-900 pr-xs font-sans
          `}
       >
-        <p className=" text-gray-600 group-hover:text-gray-900 pr-xs font-sans " tabIndex={0}>
           {props.title}
-
-        </p>
-        <div className={`chevron mt-[4px]`}>
-          <svg
-            width="8"
-            height="5"
-            viewBox="0 0 20 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M19 1L10 10L1 1"
-              stroke="#111827"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
+      
+        <Chevron/>
       </div>
-      <div className={`hidden group-hover:block group-focus-within:block absolute -left-[28px] top-[45px] z-1 w-[236px]  `}>
+      <div className={`absolute -left-[28px] top-[45px] z-1 w-[236px] hidden group-hover:block group-focus-within:block   `}>
         <SlideDown className={`mt-lg  bg-white`}>
           { props.subpages.map((navitem, i) => (
                 <a
