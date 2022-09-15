1585e989549f79bea9a08d6987679e554b1ef365
connorshannon <connorshannon@navapbc.com>
2022-09-15 10:59:23 -0700

    seperated animations and updated utils w debounce

---
+++ components/blocks/NewReportSection/animations.ts
@@ -0,0 +1,43 @@
+
+export const makeSlideUpAnimation = (elementId, delay) => {
+    let an = document.getElementById(elementId).animate(
+      [
+        { transform: "translateY(20%)", opacity: "0%" },
+        { transform: "translateY(0%)", opacity: "100%" },
+      ],
+      {
+        duration: 300,
+        iterations: 1,
+        fill: "forwards",
+        delay: delay,
+      }
+    );
+    an.pause();
+    return an;
+  };
+  
+
+ export const makeFadeAnimation = (elementId) => {
+    const an = document
+      .getElementById(elementId)
+      .animate([{ opacity: "100%" }, { opacity: "0%" }], {
+        duration: 200,
+        iterations: 1,
+        fill: "forwards",
+      });
+    an.pause();
+    return an;
+  };
+
+  export const makeFadeInAnimation = (elementId, delay) => {
+    const an = document
+      .getElementById(elementId)
+      .animate([{ opacity: "0%" }, { opacity: "100%" }], {
+        duration: 200,
+        iterations: 1,
+        fill: "forwards",
+        delay: delay
+      });
+    an.pause();
+    return an;
+  };
--- components/blocks/NewReportSection/utils.ts
+++ components/blocks/NewReportSection/utils.ts
@@ -68,3 +68,14 @@ export const animationHandler = ({
 
   return null;
 };
+
+
+export const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...args) => {
+      window.clearTimeout(timeoutId);
+      timeoutId = window.setTimeout(() => {
+        callback.apply(null, args);
+      }, wait);
+    };
+  }
