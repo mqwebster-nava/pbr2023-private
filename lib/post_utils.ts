0d961c0f2ca3d52159c13cf785bcfcaf33eb1581
connorshannon <connorshannon@navapbc.com>
2022-10-26 14:43:01 -0400

    re-removed registrationLink

--- lib/post_utils.ts
+++ lib/post_utils.ts
@@ -65,7 +65,6 @@ const formatImageAsset = (imgData) => {
       leadImage: ("leadImage" in post) ? formatImageAsset(post.leadImage): null,
       promoImage: ("promoImage" in post) ? formatImageAsset(post.promoImage): null,
       socialImage: ("socialImage" in post) ? formatImageAsset(post.socialImage): null,
-      registrationLink: post.registrationLink,
       eventInfo: post.eventInfo && {
         ...post.eventInfo,
         speakers: post.eventInfo.eventSpeakersCollection?.items?.map((speaker)=>{
