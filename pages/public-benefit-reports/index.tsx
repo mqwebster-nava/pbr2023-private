bc7fcebbcb04e6c502f5650df1ebc0848fedc8ac
connorshannon <connorshannon@navapbc.com>
2022-11-10 14:38:12 -0600

    added public-benefit-reports page

---
+++ pages/public-benefit-reports/index.tsx
@@ -0,0 +1,24 @@
+import { getPageDataFromContentful } from "lib/api";
+import { PageInterface } from "lib/data_models/page_interface";
+import PageTemplate from "components/templates/PageTemplate/PageTemplate";
+
+
+export default function PublicBenefitReports(props: PageInterface) {
+    return props? ( <PageTemplate {...props}> </PageTemplate>): <div>Error</div>
+
+}
+
+
+export async function getStaticProps({ params, preview = false }) {
+    const page: PageInterface = await getPageDataFromContentful(
+      {slug:"/public-benefit-reports",
+      preview: preview,
+    });
+    return {
+      props: page,
+      // Next.js will attempt to re-generate the page:
+      // - When a request comes in
+      // - At most once every 60 seconds
+      revalidate: 60, // In seconds
+    }
+  }
