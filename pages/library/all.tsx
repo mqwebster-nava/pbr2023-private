537fdfa41cbe87ee5487c19f517a79bb0e7c94c8
connorshannon <connorshannon@navapbc.com>
2022-11-22 22:43:32 -0600

    adding library a/b test

---
+++ pages/library/all.tsx
@@ -0,0 +1,41 @@
+import { getPageDataFromContentful } from "lib/api";
+import PageTemplate from "components/templates/PageTemplate/PageTemplate";
+import { PageInterface } from "lib/data_models/page_interface";
+import getPostsByContentType from "lib/contentful/getPostsByContentType";
+import React from "react";
+import { formatPosts } from "lib/formatters/formatPosts";
+import { BasicPostInterface } from "lib/data_models/post_interface";
+
+export default function AllLibrary(props:PageInterface) {
+ 
+  return (
+  <PageTemplate {...props}>
+  </PageTemplate>);
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  let res: PageInterface = await getPageDataFromContentful({slug:"/library/all",
+    preview: preview,
+  });
+ 
+  let _posts = await getPostsByContentType("Insight");
+  let posts: Array<BasicPostInterface> = formatPosts(_posts);
+  posts = posts.sort((a,b)=> new Date(b.date).getTime() - new Date(a.date).getTime());
+  res.contentBlocks = [
+    {
+      
+      __typename:"ContentBlockArticleList", // Article Feed
+      id: "news section",
+      items: posts,
+      layout:"3 card row"
+    },
+    ...res.contentBlocks
+  ]
+ return {
+   props:res,
+   // Next.js will attempt to re-generate the page:
+   // - When a request comes in
+   // - At most once every 60 seconds
+   revalidate: 60, // In seconds
+ };
+}
---
+++ pages/library/index.tsx
@@ -0,0 +1,33 @@
+import { getPageDataFromContentful } from "lib/api";
+import PageTemplate from "components/templates/PageTemplate/PageTemplate";
+import { PageInterface } from "lib/data_models/page_interface";
+import FloatingButtonBlock from "components/blocks/FloatingButtonBlock/FloatingButtonBlock";
+
+
+export default function Library(props:PageInterface) {
+ 
+  return props ? (
+    <PageTemplate {...props}>
+    </PageTemplate>
+  ) : (
+    <div>Error</div>
+  );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  // const posts: Array<BasicPostInterface> =
+  //   await ContentfulApi.getPostsByContentType("Insights");
+
+  const page: PageInterface = await getPageDataFromContentful(
+    {slug: "/library", 
+    preview: preview,
+  });
+  return {
+    props:  page,
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 60 seconds
+    revalidate: 60, // In seconds
+     // will be passed to the page component as props
+  };
+}
