fe1ecb2391234ee29803f79f6a78907f80d20061
connorshannon <connorshannon@navapbc.com>
2022-12-20 14:13:58 -0500

    fixing formatting

--- components/blocks/ListBlock/ListBlock.stories.tsx
+++ components/blocks/ListBlock/ListBlock.stories.tsx
@@ -1,12 +1,9 @@
 // Button.stories.ts|tsx
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import MarkdownComponent from 'utils/MarkdownComponent';
 import  ListBlock from './ListBlock';
 
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
   title: 'Blocks / List Block',
   component: ListBlock,
 } as ComponentMeta<typeof ListBlock>;
@@ -18,14 +15,12 @@ export const Primary = Template.bind({});
 
 Primary.args = {
     id: '4Q3d1MtucF7SYwFuGXSu3l',
-    __typename: 'ListBlock',
-    sys: { id: '4Q3d1MtucF7SYwFuGXSu3l' },
     title: 'Financial health',
-    body: `
-      - Competitive compensation with transparent salary levels\n
-      - Fully remote workforce with equitable pay no matter where you live in the US\n 
-      - 4% 401K salary match\n 
-      - 37 paid days off per year: 20 vacation days, 5 floating holidays, the day after Thanksgiving, Juneteenth, and 10 federal holidays`,
+    body:<MarkdownComponent content={ `
+      - Competitive compensation with transparent salary levels
+      - Fully remote workforce with equitable pay no matter where you live in the US
+      - 4% 401K salary match
+      - 37 paid days off per year: 20 vacation days, 5 floating holidays, the day after Thanksgiving, Juneteenth, and 10 federal holidays`}/>,
     colorTheme: 'sage'
   
 };
