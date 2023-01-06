af7fb2c13890531f3bcb326415c074a20cc800a7
connorshannon <connorshannon@navapbc.com>
2023-01-06 17:54:23 -0500

    styling patterns

--- components/custom_blocks/DEISection/DEICharts/PercentBarChartYOY.tsx
+++ components/custom_blocks/DEISection/DEICharts/PercentBarChartYOY.tsx
@@ -18,7 +18,7 @@ export const PercentBarChartYOY: React.FC<PercentBarChartYOYInterface> = ({
         <AnimatedBarChart
           key={`percent_bar_graph_${index}`}
           {...graph}
-          duration={1500}
+          //duration={1500}
         />
       ))}
     </>
@@ -28,11 +28,11 @@ export const PercentBarChartYOY: React.FC<PercentBarChartYOYInterface> = ({
 export interface AnimatedBarChartInterface {
   startingPercent: number;
   endingPercent: number;
-  textStart: string;
-  textEnd: string;
+  //textStart: string;
+  //textEnd: string;
   description: string;
   insideBarColor?: string;
-  duration: number;
+ // duration: number;
   startDelay?: number;
   endDelay?: number;
 }
@@ -40,73 +40,78 @@ export interface AnimatedBarChartInterface {
 export const AnimatedBarChart: React.FC<AnimatedBarChartInterface> = ({
   startingPercent,
   endingPercent,
-  textEnd = "2022",
-  textStart = "2021",
   description,
   insideBarColor = "sage",
-  duration = 500,
-  startDelay = 1500,
+  startDelay = 1000,
   endDelay = 1500,
 }) => {
+
+  const duration = Math.max(Math.abs(startingPercent-endingPercent)*80, 300)
+  const isIncreasing = endingPercent>startingPercent;
+  const endW = isIncreasing ? 100 : (100*(1-(startingPercent-endingPercent)/startingPercent))
+  const startW = isIncreasing ? 100*startingPercent/endingPercent : 100;
+  console.log(duration)
   const countUpRef = React.useRef(null);
-  //const [width, setWidth] = useState(startingPercent);
-  const { start, pauseResume, reset, update } = useCountUp({
+  const animationRef = React.useRef(null);
+  const { start } = useCountUp({
     ref: countUpRef,
     start: startingPercent,
     end: endingPercent,
     delay: startDelay / 1000,
     duration: duration / 1000,
+    startOnMount: false
   });
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+
 
   useEffect(() => {
     const an = makeSlideUpAnimation(
       `animation-${startingPercent}`,
-      startingPercent,
-      endingPercent,
+      startW,
+      endW,
       duration,
       startDelay
     );
+    const observer = new IntersectionObserver((entries, observer) => {
+      const entry = entries[0];
+      console.log('entry', an.playState);
+      console.log('entry.isIntersecting', entry.isIntersecting);
+      
+      if(entry.isIntersecting && an.playState == "paused"){
+        an.play()
+        start()
 
-    const startCycle = () =>{
-      const label = document.getElementById(`labelText-${startingPercent}`);
-      an.play();
-      start()
-      label.innerText = textStart;
-      sleep(startDelay).then(() => {
-        label.innerText = textEnd;
-      });
-    }
+      }
+    });
+    observer.observe(animationRef.current)
 
-    const intervalID = setInterval(() => {
-      startCycle()
-    }, startDelay + duration + endDelay);
-
-    startCycle()
-
-    return () => clearInterval(intervalID);
   }, [endingPercent, startDelay, duration, endDelay, startingPercent]);
 
   const insideBar = classNames({
     "bg-sage-700": insideBarColor === "sage",
     "bg-navy-900": insideBarColor === "navy",
-    "bg-plum-900": insideBarColor === "plum",
+  });
+  const bBar = classNames({
+    "bg-sage-300": insideBarColor === "sage",
+    "bg-navy-200": insideBarColor === "navy",
   });
   return (
     <div className="min-h-[140px] w-full mb-xl bg-sage-50">
+     <div style={{ width: `${startingPercent}%`}} className={` ${bBar} min-h-[140px] relative overflow-hidden`}>
       <div
+        ref={animationRef}
         id={`animation-${startingPercent}`}
-        className={`${insideBar} min-h-[inherit] px-2 py-4 text-white`}
-        style={{ width: `${startingPercent}%` }}
+        className={`absolute ${insideBar} min-h-[inherit] px-2 py-4 text-white z-10`}
+        style={{ width: `${startW}%` }}
       >
-        <p id={`labelText-${startingPercent}`}>2021</p>
-        <p className="type-preset-3 font-black inline-block">
+        <p className="type-preset-3 font-sans font-bold inline-block">
           <span ref={countUpRef} >{startingPercent}</span>
           %
         </p>
-        <p>{description}</p>
+        <p>{description} </p>
+      </div>
+      <div  className={` absolute top-0  right-0 bottom-0 opacity-90 `}>
+        <Pattern/>
+       </div>
       </div>
     </div>
   );
@@ -114,16 +119,23 @@ export const AnimatedBarChart: React.FC<AnimatedBarChartInterface> = ({
 
 const makeSlideUpAnimation = (
   elementId,
-  startingPercent,
-  endingPercent,
+  startW,
+  endW,
   duration,
   delay
 ) => {
-  console.log(startingPercent);
+  /*
+  portion of the whole box
+  [___|_]
+  56 % of 100 = x % of 73
+  56/73 = x/73
+  x = 56*73 / 100
+  */
+  
   let an = document
     .getElementById(elementId)
     .animate(
-      [{ width: `${startingPercent}%` }, { width: `${endingPercent}%` }],
+      [{ width: `${startW}%` }, { width: `${endW}%` }],
       {
         duration: duration,
         fill: "forwards",
@@ -135,6 +147,83 @@ const makeSlideUpAnimation = (
   return an;
 };
 
+
+
+const Pattern = () => {
+  return (<svg width="401" height="401" viewBox="0 0 401 401" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g clip-path="url(#clip0_573_10495)">
+  <path d="M-343.837 367.206L367.206 -343.837" stroke="white" stroke-width="2.00196"/>
+  <path d="M-332.042 379.001L379.001 -332.042" stroke="white" stroke-width="2.00196"/>
+  <path d="M-320.305 390.794L390.794 -320.305" stroke="white" stroke-width="2.00196"/>
+  <path d="M-308.51 402.589L402.589 -308.51" stroke="white" stroke-width="2.00196"/>
+  <path d="M-296.717 414.326L414.326 -296.717" stroke="white" stroke-width="2.00196"/>
+  <path d="M-284.922 426.121L426.121 -284.922" stroke="white" stroke-width="2.00196"/>
+  <path d="M-273.184 437.915L437.915 -273.184" stroke="white" stroke-width="2.00196"/>
+  <path d="M-261.389 449.71L449.71 -261.389" stroke="white" stroke-width="2.00196"/>
+  <path d="M-249.595 461.448L461.448 -249.595" stroke="white" stroke-width="2.00196"/>
+  <path d="M-237.802 473.241L473.241 -237.802" stroke="white" stroke-width="2.00196"/>
+  <path d="M-226.062 485.037L485.037 -226.062" stroke="white" stroke-width="2.00196"/>
+  <path d="M-214.269 496.83L496.83 -214.269" stroke="white" stroke-width="2.00196"/>
+  <path d="M-202.474 508.625L508.625 -202.474" stroke="white" stroke-width="2.00196"/>
+  <path d="M-190.679 520.364L520.364 -190.679" stroke="white" stroke-width="2.00196"/>
+  <path d="M-178.887 532.156L532.156 -178.887" stroke="white" stroke-width="2.00196"/>
+  <path d="M-167.146 543.952L543.952 -167.146" stroke="white" stroke-width="2.00196"/>
+  <path d="M-155.354 555.745L555.745 -155.354" stroke="white" stroke-width="2.00196"/>
+  <path d="M-143.559 567.484L567.484 -143.559" stroke="white" stroke-width="2.00196"/>
+  <path d="M-131.764 579.279L579.279 -131.764" stroke="white" stroke-width="2.00196"/>
+  <path d="M-120.025 591.073L591.073 -120.025" stroke="white" stroke-width="2.00196"/>
+  <path d="M-108.231 602.867L602.867 -108.231" stroke="white" stroke-width="2.00196"/>
+  <path d="M-96.4385 614.604L614.604 -96.4385" stroke="white" stroke-width="2.00196"/>
+  <path d="M-84.6436 626.399L626.399 -84.6436" stroke="white" stroke-width="2.00196"/>
+  <path d="M-72.9053 638.193L638.193 -72.9053" stroke="white" stroke-width="2.00196"/>
+  <path d="M-61.1104 649.988L649.988 -61.1104" stroke="white" stroke-width="2.00196"/>
+  <path d="M-49.3184 661.726L661.724 -49.3164" stroke="white" stroke-width="2.00196"/>
+  <path d="M-37.5234 673.519L673.519 -37.5234" stroke="white" stroke-width="2.00196"/>
+  <path d="M-25.7852 685.313L685.313 -25.7852" stroke="white" stroke-width="2.00196"/>
+  <path d="M-13.9902 697.108L697.108 -13.9902" stroke="white" stroke-width="2.00196"/>
+  <path d="M-2.19531 708.903L708.903 -2.19531" stroke="white" stroke-width="2.00196"/>
+  <path d="M9.59961 720.642L720.642 9.59961" stroke="white" stroke-width="2.00196"/>
+  <path d="M21.3916 732.434L732.434 21.3916" stroke="white" stroke-width="2.00196"/>
+  <path d="M33.1299 744.228L744.228 33.1299" stroke="white" stroke-width="2.00196"/>
+  </g>
+  <defs>
+  <clipPath id="clip0_573_10495">
+  <rect width="400.392" height="400.392" fill="white"/>
+  </clipPath>
+  </defs>
+  </svg>
+  )
+}
+
+  // function sleep(ms) {
+  //   return new Promise((resolve) => setTimeout(resolve, ms));
+  // }
+
+    //an.play();
+
+    // const startCycle = () =>{
+    //   //const label = document.getElementById(`labelText-${startingPercent}`);
+    //   an.play();
+    //   start()
+    //   //label.innerText = textStart;
+    //   sleep(startDelay).then(() => {
+    //     //label.innerText = "";
+    //     sleep(duration).then(()=>{
+    //       //label.innerText = textEnd;
+
+    //     })
+    //   })
+    // }
+
+    // const intervalID = setInterval(() => {
+    //   startCycle()
+    // }, startDelay + duration + endDelay);
+
+    // startCycle()
+
+    //return () => clearInterval(intervalID);
+
+//<span id={`labelText-${startingPercent}`}>2021</span>
 //   (function loop() {
 
 //       sleep(1000).then(()=>
