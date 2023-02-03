ab92d67299d137dd95c5c2e973b28eb3c4141674
connorshannon <connorshannon@navapbc.com>
2023-02-03 10:41:41 -0700

    moved contact component to contentful

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,12 +7,8 @@ import { PageInterface } from "lib/data_models/page_interface";
 
 //TODO potentially add a captcha https://developers.google.com/recaptcha/docs/v3
 export default function Contact(props: PageInterface) {
- 
-
   return (
-    <PageTemplate {...props}>
-      <ContactSection/>
-    </PageTemplate>
+    <PageTemplate {...props}/>
   );
 }
 
