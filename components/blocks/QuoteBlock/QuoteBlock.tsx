c874d3125da6d9a1365805aa09f6d69562ae5cc5
mqwebster-nava <marqueswebster@navapbc.com>
2023-04-28 16:01:00 -0500

    quote block plum variant color updated to plum-600

--- components/blocks/QuoteBlock/QuoteBlock.tsx
+++ components/blocks/QuoteBlock/QuoteBlock.tsx
@@ -3,39 +3,40 @@ import Image from "next/image";
 import QuotePattern from "public/images/quote-marks-gold-900.png";
 import QuotePatternPlum from "public/images/quote-marks-plum-300.png";
 
-type QuoteBlockVariant = "Nava Labs(Plum)"| "Default";
+type QuoteBlockVariant = "Nava Labs(Plum)" | "Default";
 
 interface QuoteBlockInterface {
   id?: string;
   body: string;
   authorName: string;
   authorRole: string;
-  variant?:QuoteBlockVariant;
+  variant?: QuoteBlockVariant;
 }
 const QuoteBlock = ({
   body,
   authorName,
   authorRole,
-  variant="Default",
+  variant = "Default",
 }: QuoteBlockInterface) => {
-
- const bgColor = classNames ({
-    "bg-gold-700": variant !="Nava Labs(Plum)",
-    "bg-purple-700": variant =="Nava Labs(Plum)"
-  })
-  const textColor = classNames ({
-    "text-gray-900": variant !="Nava Labs(Plum)",
-    "text-white": variant =="Nava Labs(Plum)"
-  })
+  const bgColor = classNames({
+    "bg-gold-700": variant != "Nava Labs(Plum)",
+    "bg-plum-600": variant == "Nava Labs(Plum)",
+  });
+  const textColor = classNames({
+    "text-gray-900": variant != "Nava Labs(Plum)",
+    "text-white": variant == "Nava Labs(Plum)",
+  });
 
   return (
     <blockquote className={`${bgColor}`}>
       <div className="responsive-container pt-2xl pb-3xl md:pb-4xl md:pt-3xl grid gap-x-2xl grid-cols-6">
-        <div className={`col-span-full md:col-span-1 pb-3xl 
+        <div
+          className={`col-span-full md:col-span-1 pb-3xl 
         h-[56px] w-[100px] lg:h-[72px] lg:w-[128px] 
-        `}>
+        `}
+        >
           <Image
-            src={variant =="Nava Labs(Plum)" ? QuotePatternPlum: QuotePattern}
+            src={variant == "Nava Labs(Plum)" ? QuotePatternPlum : QuotePattern}
             height={70}
             width={128}
             layout={"responsive"}
