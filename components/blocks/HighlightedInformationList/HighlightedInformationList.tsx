3fcf7d305dd2e8dcb17850bc1deda6b0d2d330e9
mqwebster-nava <marqueswebster@navapbc.com>
2023-04-28 16:29:48 -0500

    Highlighted info list color updated for Nava Labs

--- components/blocks/HighlightedInformationList/HighlightedInformationList.tsx
+++ components/blocks/HighlightedInformationList/HighlightedInformationList.tsx
@@ -1,4 +1,3 @@
-
 import HighlightedInfoCard from "components/atom/HighlightedInfoCard/HighlightedInfoCard";
 
 /*
@@ -8,30 +7,37 @@ for the different types of cards used throughout the site.
 
 interface HighlightedInformationListInterface {
   id: string;
+  colorTheme?: string;
   items: any;
 }
 
 const HighlightedInformationList = ({
   id,
+  colorTheme,
   items,
 }: HighlightedInformationListInterface) => {
   items = items.filter((item) => item != null);
   return (
-    <section className={`bg-purple-900`} key={id}>
-    <div className={`responsive-container pt-2xl pb-3xl`} key={id}>
-      <div className={`w-full`}>
-        <div className={`w-full grid grid-cols-1 md:grid-cols-3 gap-x-md md:gap-x-2xl gap-y-xl `}>
-          {items.map((item, i) => {
-            return (
-              <div className={`w-full self-stretch `} key={item.id}>
-                  <HighlightedInfoCard {...item}/>
-              </div>
-            );
-          })}
+    <section
+      className={colorTheme == "Nava Labs" ? `bg-plum-900` : `bg-purple-900`}
+      key={id}
+    >
+      <div className={`responsive-container pt-2xl pb-3xl`} key={id}>
+        <div className={`w-full`}>
+          <div
+            className={`w-full grid grid-cols-1 md:grid-cols-3 gap-x-md md:gap-x-2xl gap-y-xl `}
+          >
+            {items.map((item, i) => {
+              return (
+                <div className={`w-full self-stretch `} key={item.id}>
+                  <HighlightedInfoCard {...item} />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 };
 
