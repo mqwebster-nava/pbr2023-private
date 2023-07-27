8aac178313303c43aa700acc159989fd50b7311a
mqwebster-nava <marqueswebster@navapbc.com>
2023-07-27 10:06:21 -0500

    updated subtext alignment and border hover color

--- components/blocks_reports/report_2022/SectionIntro.tsx
+++ components/blocks_reports/report_2022/SectionIntro.tsx
@@ -4,42 +4,44 @@ import classNames from "classnames";
 import ArrowDown from "./Atoms/ArrowDown";
 
 const SectionIntro = ({ section, i }) => {
-  const colorTheme = section.colorTheme;
-  const bg =
-    section.colorTheme === "gold"
-      ? `bg-${colorTheme}-dark`
-      : `bg-${colorTheme}-900`;
-
   const hoverStyles = classNames({
-    "hover:text-gold-900": section.colorTheme == "gold",
-    "hover:text-plum-500": section.colorTheme == "plum",
-    "hover:text-sage-600": section.colorTheme == "sage",
-    "hover:text-purple-600": section.colorTheme == "purple",
-    "hover:text-navy-600": section.colorTheme == "navy",
+    "group-hover:text-gold-900": section.colorTheme == "gold",
+    "group-hover:text-plum-500": section.colorTheme == "plum",
+    "group-hover:text-sage-600": section.colorTheme == "sage",
+    "group-hover:text-purple-600": section.colorTheme == "purple",
+    "group-hover:text-navy-600": section.colorTheme == "navy",
+  });
+
+  const borderStyles = classNames({
+    // "hover:border-t-2 hover:border-gold-900": section.colorTheme == "gold",
+    "hover:border-t-2 hover:border-plum-500": section.colorTheme == "plum",
+    "hover:border-t-2 hover:border-sage-600": section.colorTheme == "sage",
+    "hover:border-t-2 hover:border-purple-600": section.colorTheme == "purple",
+    "hover:border-t-2 hover:border-navy-600": section.colorTheme == "navy",
   });
 
   return (
     <section
       id={`${section.anchor}`}
-      className={`pb-8 ${
+      className={`group pb-8 ${
         section.anchor == "gov-services" ? "" : "border-t-2"
-      } border-gray-200"`}
+      } border-gray-200 ${borderStyles}`}
       tabIndex={0}
     >
       <div className="responsive-container">
         <div
-          className={`flex flex-row justify-between w-full text-gray-300 ${hoverStyles} hover:cursor-pointer group`}
+          className={`flex flex-row justify-between items-baseline w-full text-gray-300 ${hoverStyles} group-hover:cursor-pointer`}
         >
           <span className="text-7xl font-sans font-black spacing-y-sm">
             {section.title}
           </span>
 
           <span
-            className={`opacity-0 group-hover:opacity-100 min-w-max min-h-full font-serif text-lg pt-11 bg-navy-50`}
+            className={`opacity-0 group-hover:opacity-100 min-w-max font-serif text-lg`}
           >
-            <div className="flex flex-row align-middle gap-1 h-fit">
+            <div className="flex flex-row items-center gap-1">
               {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
-              <ArrowDown color={section.colorTheme} />
+              <ArrowDown color={section.colorTheme} size="default" />
             </div>
           </span>
         </div>
