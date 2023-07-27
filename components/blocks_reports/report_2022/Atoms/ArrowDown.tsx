fed1689f619137b9865ee23cebd2510bdac2709d
mqwebster-nava <marqueswebster@navapbc.com>
2023-07-27 09:08:08 -0500

    added Arrow Down to atoms dir and Section Intro

---
+++ components/blocks_reports/report_2022/Atoms/ArrowDown.tsx
@@ -0,0 +1,52 @@
+import classNames from "classnames";
+
+type ArrowSize = 'large' | "default";
+
+export interface ArrowDownInterface {
+    size?: ArrowSize;
+    color?: string ;
+    sectionId?;
+    isAnimated?: boolean;
+    isMobileHidden?: boolean;
+}
+
+const ArrowDown = ({size="default", color, sectionId, isAnimated=false, isMobileHidden=false} : ArrowDownInterface) => {
+    const w = size == "default" ? "24" : "48";
+    const h = w;
+
+    const strokeStyles = classNames({
+        "stroke-gold-900": color == "gold",
+        "stroke-plum-500": color == "plum",
+        "stroke-sage-600": color == "sage",
+        "stroke-purple-600": color == "purple",
+        "stroke-navy-600": color == "navy",
+    });
+
+    console.log(color)
+
+  return (
+    <div className={`w-[${w}px] h-[${h}px]`}>
+      <svg
+        width={w}
+        height={h}
+        className={strokeStyles}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 11.6328L11.9992 19.632L19.9984 11.6328"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
+        <path
+          d="M11.998 18.9965V2.82031"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default ArrowDown
--- components/blocks_reports/report_2022/SectionIntro.tsx
+++ components/blocks_reports/report_2022/SectionIntro.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import classNames from "classnames";
 
+import ArrowDown from "./Atoms/ArrowDown";
+
 const SectionIntro = ({ section, i }) => {
   const colorTheme = section.colorTheme;
   const bg =
@@ -19,16 +21,27 @@ const SectionIntro = ({ section, i }) => {
   return (
     <section
       id={`${section.anchor}`}
-      className={`pb-8 ${section.anchor == 'gov-services' ? '' : 'border-t-2'} border-gray-200"`}
+      className={`pb-8 ${
+        section.anchor == "gov-services" ? "" : "border-t-2"
+      } border-gray-200"`}
       tabIndex={0}
     >
       <div className="responsive-container">
-        <div className={`flex flex-row justify-between w-full text-gray-300 ${hoverStyles} hover:cursor-pointer group`}>
+        <div
+          className={`flex flex-row justify-between w-full text-gray-300 ${hoverStyles} hover:cursor-pointer group`}
+        >
           <span className="text-7xl font-sans font-black spacing-y-sm">
             {section.title}
           </span>
 
-          <span className={`opacity-0 group-hover:opacity-100 min-w-max min-h-full font-serif text-lg pt-11`}>See all stories</span>
+          <span
+            className={`opacity-0 group-hover:opacity-100 min-w-max min-h-full font-serif text-lg pt-11 bg-navy-50`}
+          >
+            <div className="flex flex-row align-middle gap-1 h-fit">
+              {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
+              <ArrowDown color={section.colorTheme} />
+            </div>
+          </span>
         </div>
       </div>
     </section>
