cabbdb80639570bc473e4f005106a02da2c74335
mqwebster-nava <marqueswebster@navapbc.com>
2023-07-31 11:01:52 -0500

    content disp functionality using Details HTML el

--- components/blocks_reports/report_2022/SectionIntro.tsx
+++ components/blocks_reports/report_2022/SectionIntro.tsx
@@ -6,8 +6,6 @@ import { LinkListItem } from "./Atoms/LinkListItem";
 import ArrowDown from "./Atoms/ArrowDown";
 
 const SectionIntro = ({ section, i }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
   const openStyles = classNames({
     "text-gold-900": section.colorTheme == "gold",
     "text-plum-500": section.colorTheme == "plum",
@@ -16,7 +14,7 @@ const SectionIntro = ({ section, i }) => {
     "text-navy-600": section.colorTheme == "navy",
   });
 
-  const hoverStyles = !isOpen && classNames({
+  const hoverStyles = classNames({
     "group-hover:text-gold-900": section.colorTheme == "gold",
     "group-hover:text-plum-500": section.colorTheme == "plum",
     "group-hover:text-sage-600": section.colorTheme == "sage",
@@ -24,7 +22,7 @@ const SectionIntro = ({ section, i }) => {
     "group-hover:text-navy-600": section.colorTheme == "navy",
   });
 
-  const borderStyles = !isOpen && classNames({
+  const borderStyles = classNames({
     // "hover:border-t-2 hover:border-gold-900": section.colorTheme == "gold",
     "hover:border-t-2 hover:border-plum-500": section.colorTheme == "plum",
     "hover:border-t-2 hover:border-sage-600": section.colorTheme == "sage",
@@ -43,32 +41,30 @@ const SectionIntro = ({ section, i }) => {
   return (
     <section
       id={`${section.anchor}`}
-      className={`group flex flex-col gap-8 pb-8 ${
+      className={`group pb-8 ${
         section.anchor == "gov-services" ? "" : "border-t-2"
       } border-gray-200 ${borderStyles}`}
       tabIndex={0}
-      onClick={() => setIsOpen(!isOpen)}
     >
-      <div className="responsive-container w-full">
-        <div
-          className={`flex flex-row justify-between items-baseline ${isOpen ? openStyles : 'text-gray-300'} ${hoverStyles} group-hover:cursor-pointer`}
-        >
-          <span className="text-7xl tracking-[0.015em] font-sans font-black mt-[-15px]">
-            {section.title}
-          </span>
-
-          <span
-            className={`opacity-0 ${!isOpen && 'group-hover:opacity-100'} min-w-max font-serif text-lg`}
+      <details open={false} className="responsive-container w-full">
+        <summary className="list-none">
+          <div
+            className={`flex flex-row justify-between items-baseline text-gray-300 ${hoverStyles} group-hover:cursor-pointer`}
           >
-            <div className="flex flex-row items-center gap-1">
-              {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
-              <ArrowDown color={section.colorTheme} size="default" />
-            </div>
-          </span>
-        </div>
-      </div>
+            <span className="text-7xl tracking-[0.015em] font-sans font-black mt-[-15px]">
+              {section.title}
+            </span>
+            <span
+              className={`opacity-0 group-hover:opacity-100 min-w-max font-serif text-lg`}
+            >
+              <div className="flex flex-row items-center gap-1">
+                {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
+                <ArrowDown color={section.colorTheme} size="default" />
+              </div>
+            </span>
+          </div>
+        </summary>
 
-      {isOpen &&
         <div className={`h-[100vh]`}>
           <div className={`responsive-container`}>
             <div className={`w-full font-serif text-3xl font-light ${openStyles}`}>
@@ -94,7 +90,7 @@ const SectionIntro = ({ section, i }) => {
                 </ul>
           </div>
         </div>
-      }
+      </details>
     </section>
   );
 };
