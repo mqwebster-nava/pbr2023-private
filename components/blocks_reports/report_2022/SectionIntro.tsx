afb9f63878ecbdcc4effc55422b67667d52b7fbd
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-01 16:37:23 -0500

    one Section Intro shown at a time when open

--- components/blocks_reports/report_2022/SectionIntro.tsx
+++ components/blocks_reports/report_2022/SectionIntro.tsx
@@ -1,33 +1,13 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import classNames from "classnames";
 import MarkdownComponent from "utils/MarkdownComponent";
 
 import { LinkListItem } from "./Atoms/LinkListItem";
 import ArrowDown from "./Atoms/ArrowDown";
 
-const SectionIntro = ({ section, i }) => {
-  const refSection = useRef<HTMLDetailsElement>(null);
-  useEffect(() => {
-    const sectionIntros = document.querySelectorAll('details');
-
-    const sectionIntroIsOpen = (target: HTMLDetailsElement) => {
-      sectionIntros.forEach((current: HTMLDetailsElement) => {
-        if (current != target) {
-          current.removeAttribute("open");
-        }
-      })
-    }
-
-    sectionIntros.forEach((target) => {
-      target.addEventListener('click', () => sectionIntroIsOpen(target));
-    })
-
-    return () => {
-      sectionIntros.forEach((target) => {
-        target.removeEventListener('click', () => sectionIntroIsOpen(target));
-      })
-    }
-  }, [])
+const SectionIntro = ({ section, i, currentlyOpenSection, setOpenSection }) => {
+  const isOpen = section.anchor == currentlyOpenSection;
+  const isHidden = (section.anchor !== currentlyOpenSection) && (currentlyOpenSection !== null)
 
   const openStyles = classNames({
     "text-gold-900": section.colorTheme == "gold",
@@ -37,7 +17,7 @@ const SectionIntro = ({ section, i }) => {
     "text-navy-600": section.colorTheme == "navy",
   });
 
-  const hoverStyles = classNames({
+  const hoverStyles = !isOpen && classNames({
     "group-hover:text-gold-900": section.colorTheme == "gold",
     "group-hover:text-plum-500": section.colorTheme == "plum",
     "group-hover:text-sage-600": section.colorTheme == "sage",
@@ -45,7 +25,7 @@ const SectionIntro = ({ section, i }) => {
     "group-hover:text-navy-600": section.colorTheme == "navy",
   });
 
-  const borderStyles = classNames({
+  const borderStyles = !isOpen && classNames({
     // "hover:border-t-2 hover:border-gold-900": section.colorTheme == "gold",
     "hover:border-t-2 hover:border-plum-500": section.colorTheme == "plum",
     "hover:border-t-2 hover:border-sage-600": section.colorTheme == "sage",
@@ -64,43 +44,43 @@ const SectionIntro = ({ section, i }) => {
   return (
     <section
       id={`${section.anchor}`}
-      className={`group pb-8 ${
-        section.anchor == "gov-services" ? "" : "border-t-2"
+      className={`${isHidden ? "hidden" : "group pb-8"} ${
+        i == 1 ? "" : "border-t-2"
       } border-gray-200 ${borderStyles}`}
       tabIndex={0}
+      onClick={() => setOpenSection(isOpen ? null : section.anchor)}
     >
-      <details open={false} ref={refSection} className="responsive-container w-full">
-        <summary className="list-none">
-          <div
-            className={`flex flex-row justify-between items-baseline text-gray-300 ${hoverStyles} group-hover:cursor-pointer`}
-          >
-            <span className="text-7xl tracking-[0.015em] font-sans font-black mt-[-15px]">
-              {section.title}
-            </span>
-            <span
-              className={`opacity-0 group-hover:opacity-100 min-w-max font-serif text-lg`}
-            >
-              <div className="flex flex-row items-center gap-1">
-                {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
-                <ArrowDown color={section.colorTheme} size="default" />
-              </div>
-            </span>
-          </div>
-        </summary>
+      <div className="responsive-container w-full">
+        <div
+          className={`flex flex-row justify-between items-baseline ${isOpen ? openStyles : 'text-gray-300'} ${hoverStyles} group-hover:cursor-pointer`}
+        >
+          <span className="text-7xl tracking-[0.015em] font-sans font-black mt-[-15px]">
+            {section.title}
+          </span>
 
-        <div className={`h-[100vh]`}>
+          <span
+            className={`opacity-0 ${!isOpen && 'group-hover:opacity-100'} min-w-max font-serif text-lg`}
+          >
+            <div className="flex flex-row items-center gap-1">
+              {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
+              <ArrowDown color={section.colorTheme} size="default" />
+            </div>
+          </span>
+        </div>
+      </div>
+
+      {isOpen &&
+        <div className={`h-full`}>
           <div className={`responsive-container`}>
-            <div
-              className={`w-full font-serif text-3xl font-light ${openStyles}`}
-            >
+            <div className={`w-full font-serif text-3xl font-light ${openStyles}`}>
               <MarkdownComponent content={section.body} />
             </div>
-            <ul className={``}>
-              {section.items
-                .filter((story) => story.hideStory !== true)
-                .map((story) => {
-                  return (
-                    <LinkListItem
+            <ul
+                  className={``}
+                >
+                  {section.items.filter((story)=>story.hideStory!==true).map((story) => {
+                    return (
+                      <LinkListItem
                       key={`sectionIntro-${story.anchor}`}
                       href={`#${section.anchor}--${story.anchor}`}
                       variant={"default"}
@@ -110,12 +90,12 @@ const SectionIntro = ({ section, i }) => {
                     >
                       {story.title}
                     </LinkListItem>
-                  );
-                })}
-            </ul>
+                    );
+                  })}
+                </ul>
           </div>
         </div>
-      </details>
+      }
     </section>
   );
 };
