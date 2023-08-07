d7c8d5465a331f9b2e1cb20497f5983e3032687f
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-07 16:44:41 -0500

    hide header content when story open

--- components/blocks_reports/report_2022/ReportHero2022.tsx
+++ components/blocks_reports/report_2022/ReportHero2022.tsx
@@ -1,17 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PageHeaderInterface } from "components/blocks/PageHeaders/PageHeader";
 
 const ReportHero2022 = (
   { openStory },
   {}: PageHeaderInterface
 ) => {
+  const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+    setIsOpen(openStory != null);
+  }, [openStory])
+
   return (
     <header className={`w-full`} id="reportHeader">
+    {!isOpen &&
       <div className="responsive-container pt-8">
         <span className=" text-7xl font-sans font-black spacing-y-sm">
           Envisioning...
         </span>
       </div>
+    }
     </header>
   );
 };
