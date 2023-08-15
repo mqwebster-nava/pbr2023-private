3a5c2c9292d795e31b767a81f929d5de7c38e660
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-15 12:19:31 -0500

    moved Stories Dropdown Menu to separate file

---
+++ components/blocks_reports/report_2022/Atoms/StoriesDropdownMenu.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const StoriesDropdownMenu = ({items, colorTheme, sectionAnchor, openStory, setOpenStory}) => {
+    const [selectedOption, setSelectedOption] = useState(items[0].title);
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    const selectOption = (option) => {
+      setSelectedOption(option);
+      setOpenStory(option);
+      setIsOpen(false);
+    };
+  
+    const availableOptions = items.filter((option) => option.title !== selectedOption);
+  
+    return (
+      <div className={`relative z-40 w-full font-serif font-semibold type-preset-3 text-${colorTheme}-900 border-b-[1px] border-${colorTheme}-900`}>
+        <div className={`w-full ${isOpen ? `bg-white hover:bg-${colorTheme}-50` : `bg-${colorTheme}-50`}`}>
+          <div className={`responsive-container`}>
+            <button
+              type="button"
+              className={`inline-flex w-full py-md focus:outline-none focus:ring-none`}
+              id="dropdown-menu-button"
+              onClick={toggleDropdown}
+            >
+              <div className={``}>
+                {selectedOption}
+              </div>
+            </button>
+          </div>
+        </div>
+  
+        {isOpen && (
+          <div className={`absolute w-full border-b-[1px] border-${colorTheme}-900`}>
+            <div className={``} role="menu" aria-orientation="vertical" aria-labelledby="dropdown-menu-button">
+              {availableOptions.map((option, index) => (
+                <div key={index} className={`w-full border-t-[1px] border-${colorTheme}-900 bg-white hover:bg-${colorTheme}-50`}>
+                  <a
+                    href={`2022#${sectionAnchor}--${option.anchor}`}
+                    className="flex w-full py-md responsive-container"
+                    role="menuitem"
+                    onClick={() => selectOption(option.title)}
+                  >
+                    {option.title}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  export default StoriesDropdownMenu;
