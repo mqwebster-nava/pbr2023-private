7438027d01afa7f277b3aa59544cbab87df737e0
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-15 12:52:55 -0500

    no change

--- pages/nava-resources.tsx
+++ pages/nava-resources.tsx
@@ -3,8 +3,10 @@ import PageTemplate from "components/templates/PageTemplate/PageTemplate";
 import { PageInterface } from "lib/data_models/page_interface";
 //https://docs.google.com/document/d/1SWVZyiBQ0xPpmFcVuZFwyqtYOQlV5ujWZejTJ4d8Cs4/edit#
 
-export default function NavaResources(props:PageInterface ) {
-  return props? (<PageTemplate {...props}/>): <div>Error</div>;
+export default function NavaResources(props: PageInterface) {
+  return (
+    <PageTemplate {...props}/>
+  );
 }
 
 export async function getStaticProps({ params, preview = false }) {
