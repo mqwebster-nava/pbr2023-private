27735c8892afc5369b6a007ddcc84d4c801151ca
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-15 12:38:00 -0500

    added Nava Resources test page

---
+++ pages/nava-resources.tsx
@@ -0,0 +1,19 @@
+import { getPageDataFromContentful } from "lib/api";
+import PageTemplate from "components/templates/PageTemplate/PageTemplate";
+import { PageInterface } from "lib/data_models/page_interface";
+//https://docs.google.com/document/d/1SWVZyiBQ0xPpmFcVuZFwyqtYOQlV5ujWZejTJ4d8Cs4/edit#
+
+export default function NavaResources(props:PageInterface ) {
+  return props? (<PageTemplate {...props}/>): <div>Error</div>;
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const res: PageInterface = await getPageDataFromContentful({
+    slug:"/nava-resources",
+    preview: preview,
+  });
+ return {
+   props:res,
+   revalidate: 60, // In seconds
+ };
+}
