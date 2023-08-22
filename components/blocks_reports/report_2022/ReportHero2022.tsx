28a68d8444662e5a66ba0bae08213c385d0f389e
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-22 10:03:40 -0500

    removed Envisioning in Report Hero when story open

--- components/blocks_reports/report_2022/ReportHero2022.tsx
+++ components/blocks_reports/report_2022/ReportHero2022.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import { PageHeaderInterface } from "components/blocks/PageHeaders/PageHeader";
 
-const ReportHero2022 = ({}: PageHeaderInterface) => {
+const ReportHero2022 = ({activeStory}, {}: PageHeaderInterface) => {
   return (
-    <header className={`w-full`} id="reportHeader">
-      <div className="responsive-container pt-8">
-        <span className=" text-7xl font-sans font-black spacing-y-sm">
-          Envisioning...
-        </span>
-      </div>
+    <header className={`w-full ${activeStory ? 'hidden' : ''}`} id="reportHeader">
+      {!activeStory &&
+        <div className="responsive-container pt-8">
+          <span className=" text-7xl font-sans font-black spacing-y-sm">
+            Envisioning...
+          </span>
+        </div>
+      }
     </header>
   );
 };
