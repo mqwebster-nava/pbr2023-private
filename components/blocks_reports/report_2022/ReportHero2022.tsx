a76f7600da3170c0c37f58be63881dff11622a65
mqwebster-nava <marqueswebster@navapbc.com>
2023-08-23 09:31:44 -0500

    updated section intro colors

--- components/blocks_reports/report_2022/ReportHero2022.tsx
+++ components/blocks_reports/report_2022/ReportHero2022.tsx
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 import { PageHeaderInterface } from "components/blocks/PageHeaders/PageHeader";
 
-const ReportHero2022 = ({reportSections, activeSection, activeStory}, {}: PageHeaderInterface) => {
-  let currentSection = reportSections.find((section) => {if (section.anchor == activeSection) {return section}})
+const ReportHero2022 = (
+  { reportSections, activeSection, activeStory },
+  {}: PageHeaderInterface
+) => {
+  let currentSection = reportSections.find((section) => {
+    if (section.anchor == activeSection) {
+      return section;
+    }
+  });
 
   return (
-    <header className={`w-full ${activeSection && `bg-${currentSection.colorTheme}-900 text-white`} ${activeStory ? 'hidden' : ''}`} id="reportHeader">
-      {!activeStory &&
+    <header
+      className={`w-full ${
+        activeSection ? currentSection.colorTheme == 'gold' ? `bg-gold-pbrcustomdark text-white` : `bg-${currentSection.colorTheme}-900 text-white` : ''
+      } ${activeStory ? "hidden" : ""}`}
+      id="reportHeader"
+    >
+      {!activeStory && (
         <div className="responsive-container pt-8 pb-2">
           <span className=" text-7xl font-sans font-black spacing-y-sm">
             Envisioning...
           </span>
         </div>
-      }
+      )}
     </header>
   );
 };
--- components/blocks_reports/report_2022/SectionIntro.tsx
+++ components/blocks_reports/report_2022/SectionIntro.tsx
@@ -40,83 +40,106 @@ const SectionIntro = ({
     setActiveStory(null);
   };
 
-  const openStyles = isSectionOpen ? classNames({
-    "text-white bg-gold-pbrcustomdark": section.colorTheme == "gold",
-    "text-white bg-plum-900": section.colorTheme == "plum",
-    "text-white bg-sage-900": section.colorTheme == "sage",
-    "text-white bg-purple-900": section.colorTheme == "purple",
-    "text-white bg-navy-900": section.colorTheme == "navy",
-  }) : `text-gray-300`;
+  const openStyles = isSectionOpen
+    ? classNames({
+        "text-white bg-gold-pbrcustomdark": section.colorTheme == "gold",
+        "text-white bg-plum-900": section.colorTheme == "plum",
+        "text-white bg-sage-900": section.colorTheme == "sage",
+        "text-white bg-purple-900": section.colorTheme == "purple",
+        "text-white bg-navy-900": section.colorTheme == "navy",
+      })
+    : `text-gray-300`;
 
-  const bgStyles = !isSectionOpen ? classNames({
-      "hover:bg-gold-pbrcustomdark": section.colorTheme == "gold",
-      "hover:bg-plum-900": section.colorTheme == "plum",
-      "hover:bg-sage-900": section.colorTheme == "sage",
-      "hover:bg-purple-900": section.colorTheme == "purple",
-      "hover:bg-navy-900": section.colorTheme == "navy",
-    }) : ``;
+  const bgStyles = !isSectionOpen
+    ? classNames({
+        "hover:bg-gold-pbrcustomdark": section.colorTheme == "gold",
+        "hover:bg-plum-900": section.colorTheme == "plum",
+        "hover:bg-sage-900": section.colorTheme == "sage",
+        "hover:bg-purple-900": section.colorTheme == "purple",
+        "hover:bg-navy-900": section.colorTheme == "navy",
+      })
+    : ``;
 
   return (
     <section
       id={`${section.anchor}`}
-      className={`w-full group ${!activeSection ? section.themeNum == 1 ? `border-none` : `border-t-2 border-gray-300 hover:border-none` : `bg-${section.colorTheme}-900`} ${bgStyles} ${isSectionHidden ? `hidden` : ``}`}
+      className={`w-full group ${
+        !activeSection
+          ? section.themeNum == 1
+            ? `border-none`
+            : `border-t-2 border-gray-300 hover:border-none`
+          : section.colorTheme == 'gold' ?
+            `bg-gold-pbrcustomdark`
+            : `bg-${section.colorTheme}-900`
+      } ${bgStyles} ${isSectionHidden ? `hidden` : ``}`}
       tabIndex={0}
       onMouseEnter={() => {
         let hero = document.getElementById("reportHeader");
 
-        if (section.themeNum == 1) {
+        if (!activeSection && section.themeNum == 1) {
           hero.classList.add(`bg-gold-pbrcustomdark`, `text-white`);
         }
       }}
       onMouseLeave={() => {
         let hero = document.getElementById("reportHeader");
 
-        if (section.themeNum == 1) {
+        if (!activeSection && section.themeNum == 1) {
           hero.classList.remove(`bg-gold-pbrcustomdark`, `text-white`);
         }
       }}
     >
-      {!activeStory && <div className="responsive-container w-full pb-8" onClick={toggleSection}>
+      {!activeStory && (
         <div
-          className={`flex flex-row justify-between items-baseline group-hover:cursor-pointer group-hover:text-white ${openStyles}`}
+          className="responsive-container w-full pb-8"
+          onClick={toggleSection}
         >
-          <span className="text-7xl tracking-[0.015em] font-sans font-black mt-[-15px]">
-            {section.title}
-          </span>
-          <span
-            className={isSectionOpen ? `opacity-0` : `opacity-0 group-hover:opacity-100 min-w-max font-sans font-semibold type-preset-6`}
+          <div
+            className={`flex flex-row justify-between items-baseline group-hover:cursor-pointer group-hover:text-white ${openStyles}`}
           >
-            <div className="flex flex-row items-center gap-1">
-              {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
-              <ArrowDown color={'white'} size="default" />
-            </div>
-          </span>
+            <span className="text-7xl tracking-[0.015em] font-sans font-black mt-[-15px]">
+              {section.title}
+            </span>
+            <span
+              className={
+                isSectionOpen
+                  ? `opacity-0`
+                  : `opacity-0 group-hover:opacity-100 min-w-max font-sans font-semibold type-preset-6`
+              }
+            >
+              <div className="flex flex-row items-center gap-1">
+                {section.themeNum == "1" ? "Read Introduction" : "Read Stories"}
+                <ArrowDown color={"white"} size="default" />
+              </div>
+            </span>
+          </div>
         </div>
-      </div>}
+      )}
 
       <div className={`h-full ${!isSectionOpen ? `hidden` : ``}`}>
         <div className={`flex flex-col gap-8`}>
-            {!activeStory && <div
+          {!activeStory && (
+            <div
               className={`responsive-container w-full flex justify-end font-serif mt-8 text-3xl font-light ${openStyles}`}
             >
               <div className={"w-2/3"}>
                 <MarkdownComponent content={section.body} />
               </div>
-            </div>}
+            </div>
+          )}
 
           <div className={`relative`}>
             <div className={`sticky top-[109px] z-10`}>
-                <StoriesDropdownMenu
-                  items={section.items}
-                  colorTheme={section.colorTheme}
-                  sectionAnchor={section.anchor}
-                  activeStory={activeStory}
-                  setActiveStory={setActiveStory}
-                  parentSectionOpen={isSectionOpen}
-                />
+              <StoriesDropdownMenu
+                items={section.items}
+                colorTheme={section.colorTheme}
+                sectionAnchor={section.anchor}
+                activeStory={activeStory}
+                setActiveStory={setActiveStory}
+                parentSectionOpen={isSectionOpen}
+              />
             </div>
 
-            {(isSectionOpen && activeStory) &&
+            {isSectionOpen && activeStory && (
               <ul className={``}>
                 {section.items
                   .filter((story) => story.hideStory !== true)
@@ -138,7 +161,7 @@ const SectionIntro = ({
                     );
                   })}
               </ul>
-            }
+            )}
           </div>
         </div>
       </div>
