3480e8cfe18a2f8d82fe489a60743e0bf0c3e5a7
mqwebster-nava <marqueswebster@navapbc.com>
2023-09-13 19:20:28 -0500

    hover animations for nav arrows

--- components/blocks_reports/report_2022/Atoms/NavigationArrows.tsx
+++ components/blocks_reports/report_2022/Atoms/NavigationArrows.tsx
@@ -1,4 +1,5 @@
 import Arrow from "./Arrow";
+import { useRef } from "react";
 
 export interface NavigationArrowsInterface {
   color?: string;
@@ -11,20 +12,23 @@ const NavigationArrows = ({
   handleNextSection,
   handlePrevSection,
 }: NavigationArrowsInterface) => {
+  const nextArrowRef = useRef(null);
+  const prevArrowRef = useRef(null);
+
   return (
     <div className={`flex flex-col gap-8`}>
-      <button className={`flex flex-col w-5 [&:hover~*]:opacity-25`} onClick={handleNextSection}>
+      <button onMouseOver={() => prevArrowRef.current.style.opacity = "0.25"} onMouseLeave={() => prevArrowRef.current.style.opacity = "1"} ref={nextArrowRef} className={`group/nextArrow flex flex-col w-5`} onClick={handleNextSection}>
         <div className={`-rotate-90`}>
           <Arrow color={color} />
         </div>
-        <div className={``}>Next</div>
+        <div className={`group-hover/nextArrow:underline`}>Next</div>
       </button>
 
-      <button className={`flex flex-col w-5 [&:hover~*]:opacity-25`} onClick={handlePrevSection}>
+      <button onMouseOver={() => nextArrowRef.current.style.opacity = "0.25"} onMouseLeave={() => nextArrowRef.current.style.opacity = "1"} ref={prevArrowRef} className={`group/prevArrow flex flex-col w-5`} onClick={handlePrevSection}>
         <div className={`rotate-90`}>
           <Arrow color={color} />
         </div>
-        <div className={``}>Previous</div>
+        <div className={`group-hover/prevArrow:underline`}>Previous</div>
       </button>
     </div>
   );
