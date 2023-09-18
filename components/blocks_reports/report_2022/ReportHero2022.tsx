11210bac037ca3287e07c4a37f186914a96501a5
mqwebster-nava <marqueswebster@navapbc.com>
2023-09-18 17:48:47 -0500

    updated report hero to a tag

--- components/blocks_reports/report_2022/ReportHero2022.tsx
+++ components/blocks_reports/report_2022/ReportHero2022.tsx
@@ -13,16 +13,16 @@ const ReportHero2022 = ({activeSection, setActiveSection, setActiveStory}, {}: P
       <div
         className={`responsive-container w-full max-h-32 flex flex-row justify-between pt-8 pb-4 relative`}
       >
-        <div className="hover:cursor-pointer" onClick={() => {
+        <a href={`/public-benefit-reports/2022`} className={``} onClick={(e) => {
+          e.preventDefault();
           setActiveSection(null);
           setActiveStory(null);
-          router.push('/public-benefit-reports/2022')
-          
+          router.push(`/public-benefit-reports/2022`);
         }}>
           <span className=" transition-all duration-500 text-7xl font-sans font-black spacing-y-sm">
           Envisioning<span className={`transition-all duration-1000 ${activeSection ? `opacity-0` : `opacity-100`}`}>...</span>
           </span>
-        </div>
+        </a>
       </div>
     </header>
   );
